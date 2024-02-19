8b9eeb392e35cdb017f61617fe87d907919e9771
Karim Bourass <bourasskarim9@gmail.com>
2024-02-20 00:21:06 +0100

    Update file preview component with ngx-monaco-editor

--- src/app/feature/files/components/file-preview/file-preview.component.ts
+++ src/app/feature/files/components/file-preview/file-preview.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-  selector: "app-file-preview",
-  templateUrl: "./file-preview.component.html",
-  styleUrls: ["./file-preview.component.scss"],
+  selector: 'app-file-preview',
+  templateUrl: './file-preview.component.html',
+  styleUrls: ['./file-preview.component.scss'],
 })
 export class FilePreviewComponent {
-  constructor() {}
+  editorOptions = { theme: 'vs-dark', language: 'html' };
+  code: string = `
+  <body>
+  <h1>Hello, World!</h1>
+  <p>This is a simple HTML page.</p>
+  <a href="https://example.com">Visit Example.com</a>
+</body>
+  `;
 
-  @Input() code = "";
+  constructor(public sanitizer: DomSanitizer) {}
 }
--- src/app/feature/files/files.module.ts
+++ src/app/feature/files/files.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FilesComponent } from "./files.component";
-import { RouterModule, Routes } from "@angular/router";
-import { NzTableModule } from "ng-zorro-antd/table";
-import { NzDividerModule } from "ng-zorro-antd/divider";
-import { NzPageHeaderModule } from "ng-zorro-antd/page-header";
-import { NzTagModule } from "ng-zorro-antd/tag";
-import { FilePreviewComponent } from "./components/file-preview/file-preview.component";
-import { NzCodeEditorModule } from "ng-zorro-antd/code-editor";
-import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FilesComponent } from './files.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { FilePreviewComponent } from './components/file-preview/file-preview.component';
+import { NzCodeEditorModule } from 'ng-zorro-antd/code-editor';
+import { FormsModule } from '@angular/forms';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: FilesComponent,
   },
   {
-    path: ":id",
+    path: ':id',
     component: FilePreviewComponent,
   },
 ];
@@ -32,6 +33,7 @@ const routes: Routes = [
     NzPageHeaderModule,
     NzTagModule,
     NzCodeEditorModule,
+    MonacoEditorModule,
   ],
 })
 export class FilesModule {}
